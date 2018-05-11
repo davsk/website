119e014b904567924322955c9addfb1e2699b443
David Skinner <skinner.david@gmail.com>
2018-05-10 22:31:02 -0500

    polymer build successful

--- docs/src/my-view1.js
+++ docs/src/my-view1.js
@@ -1,16 +1,1 @@
-define(["./my-app.js"],function(_myApp){"use strict";class MyView1 extends _myApp.PolymerElement{static get template(){return _myApp.html`
-      <style include="shared-styles">
-        :host {
-          display: block;
-
-          padding: 10px;
-        }
-      </style>
-
-      <div class="card">
-        <div class="circle">1</div>
-        <h1>View One</h1>
-        <p>Ut labores minimum atomorum pro. Laudem tibique ut has.</p>
-        <p>Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Cu mei vide viris gloriatur, at populo eripuit sit.</p>
-      </div>
-    `}}window.customElements.define("my-view1",MyView1)});
+define(["./my-app.js"],function(_myApp){"use strict";var _templateObject_34e0eb9054cb11e8a71de92e81153f96=babelHelpers.taggedTemplateLiteral(["\n      <style include=\"shared-styles\">\n        :host {\n          display: block;\n\n          padding: 10px;\n        }\n      </style>\n\n      <div class=\"card\">\n        <div class=\"circle\">1</div>\n        <h1>View One</h1>\n        <p>Ut labores minimum atomorum pro. Laudem tibique ut has.</p>\n        <p>Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Cu mei vide viris gloriatur, at populo eripuit sit.</p>\n      </div>\n    "]),MyView1=function(_PolymerElement){babelHelpers.inherits(MyView1,_PolymerElement);function MyView1(){babelHelpers.classCallCheck(this,MyView1);return babelHelpers.possibleConstructorReturn(this,(MyView1.__proto__||Object.getPrototypeOf(MyView1)).apply(this,arguments))}babelHelpers.createClass(MyView1,null,[{key:"template",get:function get(){return(0,_myApp.html)(_templateObject_34e0eb9054cb11e8a71de92e81153f96)}}]);return MyView1}(_myApp.PolymerElement);window.customElements.define("my-view1",MyView1)});
