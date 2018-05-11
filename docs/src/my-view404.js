b9dab37fc12fdb5fa38a457d72a22c1b9c330a6a
David Skinner <skinner.david@gmail.com>
2018-05-10 19:11:36 -0500

    polymer build 1st try

---
+++ docs/src/my-view404.js
@@ -0,0 +1,31 @@
+define(["../node_modules/@polymer/polymer/polymer-element.js"], function (_polymerElement) {
+  "use strict";
+
+  /**
+   * @license
+   * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+   * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+   * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+   * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+   * Code distributed by Google as part of the polymer project is also
+   * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+   */
+  class MyView404 extends _polymerElement.PolymerElement {
+    static get template() {
+      return _polymerElement.html`
+      <style>
+        :host {
+          display: block;
+
+          padding: 10px 20px;
+        }
+      </style>
+
+      Oops you hit a 404. <a href="[[rootPath]]">Head back to home.</a>
+    `;
+    }
+
+  }
+
+  window.customElements.define('my-view404', MyView404);
+});
